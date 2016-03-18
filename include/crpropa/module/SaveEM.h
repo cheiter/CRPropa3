7834674219bed38a592e403c5e7f49459c340270
Christopher Heiter <heiter@lx3a25.physik.rwth-aachen.de>
2016-03-18 10:43:27 +0100

    Added module to write all EM particles to output file which have energy below certain threshold

---
+++ include/crpropa/module/SaveEM.h
@@ -0,0 +1,28 @@
+#ifndef CRPROPA_SAVE_EM_H
+#define CRPROPA_SAVE_EM_H
+
+#include "crpropa/Module.h"
+#include "crpropa/Units.h"
+
+#include <memory>
+#include <fstream>
+
+namespace crpropa {
+
+class SaveEM: public Module {
+private:
+	std::string filename;
+	mutable std::ofstream output;
+  mutable double Ethreshold;
+public:
+	SaveEM(const std::string &filename);
+	SaveEM(const std::string &filename, const double Ethr);
+	~SaveEM();
+	void process(Candidate *candidate) const;
+	std::string getDescription() const;
+	void endRun();
+};
+
+} // namespace crpropa
+
+#endif // CRPROPA_SAVE_EM_H
---
+++ src/module/SaveEM.cpp
@@ -0,0 +1,84 @@
+#include "crpropa/module/SaveEM.h"
+#include "crpropa/Units.h"
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace crpropa {
+
+SaveEM::SaveEM(const string &filename) :
+		filename(filename), output(filename.c_str()) {
+	output << "#ID\tE\tD\tpID\tpE\tiID\tiE\n";
+	output << "#\n";
+	output << "# ID          Id of particle (photon, electron, positron)\n";
+	output << "# E           Energy [EeV]\n";
+	output << "# D           Comoving trajectory length [Mpc]\n";
+	output << "# pID         Id of parent particle\n";
+	output << "# pE          Energy [EeV] of parent particle\n";
+	output << "# iID         Id of source particle\n";
+	output << "# iE          Energy [EeV] of source particle\n";
+	output << "#\n";
+  Ethreshold = 0.1 * EeV;
+}
+
+SaveEM::SaveEM(const string &filename, double Ethr) :
+		filename(filename), output(filename.c_str()) {
+	output << "#ID\tE\tD\tpID\tpE\tiID\tiE\n";
+	output << "#\n";
+	output << "# ID          Id of particle (photon, electron, positron)\n";
+	output << "# E           Energy [EeV]\n";
+	output << "# D           Comoving trajectory length [Mpc]\n";
+	output << "# pID         Id of parent particle\n";
+	output << "# pE          Energy [EeV] of parent particle\n";
+	output << "# iID         Id of source particle\n";
+	output << "# iE          Energy [EeV] of source particle\n";
+	output << "#\n";
+  Ethreshold = Ethr;
+}
+
+SaveEM::~SaveEM() {
+}
+
+void SaveEM::process(Candidate *candidate) const {
+	int pid = candidate->current.getId();
+	if ((pid != 22) and (abs(pid) != 11))
+		return;
+  if (candidate->isActive()==false || candidate->current.getEnergy() > Ethreshold)
+    return;
+
+	char buffer[1024];
+	size_t p = 0;
+
+	p += sprintf(buffer + p, "%4i\t", pid);
+	p += sprintf(buffer + p, "%g\t", candidate->current.getEnergy() / EeV);
+	p += sprintf(buffer + p, "%8.4f\t", candidate->created.getPosition().getR() / Mpc);
+
+	p += sprintf(buffer + p, "%10i\t", candidate->created.getId());
+	p += sprintf(buffer + p, "%8.4f\t", candidate->created.getEnergy() / EeV);
+
+	p += sprintf(buffer + p, "%10i\t", candidate->source.getId());
+	p += sprintf(buffer + p, "%8.4f\n", candidate->source.getEnergy() / EeV);
+
+
+#pragma omp critical
+	{
+		output.write(buffer, p);
+	}
+
+	candidate->setActive(false);
+}
+
+void SaveEM::endRun() {
+	output.flush();
+}
+
+string SaveEM::getDescription() const {
+	std::stringstream s;
+	s << "SaveEM: Output file = " << filename;
+	return s.str();
+}
+
+} // namespace crpropa
